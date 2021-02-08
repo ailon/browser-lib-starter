8d8f78e855b0d584db540df6e729e68f334254f0
ailon <ailon@ailon.org>
2021-02-08 15:59:18 +0200

    add jsdoc comment samples

--- src/logic/LibDemo.ts
+++ src/logic/LibDemo.ts
@@ -1,4 +1,12 @@
+/**
+ * Demo class - replace with your own code.
+ */
 export class LibDemo {
+  /**
+   * Demo method.
+   * 
+   * @param target - parent element to add a new element to.
+   */
   public addHello(target: HTMLElement): void {
     const helloBox = document.createElement('div');
     helloBox.innerHTML = 'Hello!';
